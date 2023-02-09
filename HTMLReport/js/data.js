d4634b8e1f21c869bfd38e956bfb7d8e267b34e3
Malisetty <prasanth.malisetty@capgemini.com>
2023-02-09 16:24:26 +0530

    s

---
+++ HTMLReport/js/data.js
@@ -0,0 +1,2 @@
+addLog(1672126725065, "TestResult@2@29ea78b1", "ACC_FCCGST_BL_TS_02_TC_02@2@29ea78b1", "ACC_FCCGST_BL_TS_02_TC_02", "Windows", "CHROME",  "1", "null", "START", "DetailReports/ACC_FCCGST_BL_TS_02_TC_02_CHROME.html","0")
+addLog(1672128264908, "TestResult@2@29ea78b1", "ACC_FCCGST_BL_TS_02_TC_02@2@29ea78b1", "ACC_FCCGST_BL_TS_02_TC_02", "Windows", "CHROME",  "1", "null", "FINISH", "PASSED","0")
